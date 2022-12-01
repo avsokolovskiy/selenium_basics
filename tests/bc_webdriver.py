1856a714e35a9456e33cdbec3126e254b1dbafd3
avsokolovskiy <andrey.sokolovskiy@brainly.com>
2022-12-01 17:31:58 +0100

    refactor: fix after static review

--- tests/bc_webdriver.py
+++ tests/bc_webdriver.py
@@ -3,12 +3,12 @@ import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
 
-TIME_TO_WAIT = 5000
 
+TIME_TO_WAIT = 6
+SLEEP_INT = 7
 URL = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 DRIVER_SERVICE = Service(executable_path=ChromeDriverManager().install())
 ADMIN_PASSWORD = 'admin123'
@@ -16,7 +16,7 @@ ADMIN_LOGIN = 'Admin'
 NEW_USER_NAME = 'Petr'
 NEW_USER_LASTNAME = 'Ivanoff'
 NEW_JOB_TITLE = 'QA Engineer'
-NO_SEARCH_RERSULTS = "No Records Found"
+NO_SEARCH_RESULTS = "No Records Found"
 
 
 class BCSelWebDriver(unittest.TestCase):
@@ -35,16 +35,15 @@ class BCSelWebDriver(unittest.TestCase):
         self.assertTrue(self.driver.find_element(By.XPATH, '//h6[text()="Dashboard"]'))
 
     def tearDown(self) -> None:
-        self.driver.find_element(By.XPATH, '//img[@alt="profile picture" and @class="oxd-userdropdown-img"]').click()
+        p_picture = '//img[@alt="profile picture" and @class="oxd-userdropdown-img"]'
+        self.driver.find_element(By.XPATH, p_picture).click()
         self.driver.find_element(By.XPATH, '//a[text()="Logout"]').click()
         self.assertTrue(self.driver.find_element(By.XPATH, '//h5[text()="Login"]'))
         self.driver.quit()
 
     def test_add_new_user(self) -> None:
         """Adding a new user in PIM tab """
-
-        self.find_xp('//span[text()="PIM"]').click()
-        # self.driver.find_element(By.XPATH, '//span[text()="PIM"]').click()
+        self.driver.find_element(By.XPATH, '//span[text()="PIM"]').click()
         self.driver.find_element(By.XPATH, '//button[text()=" Add "]').click()
         self.driver.find_element(By.XPATH, '//input[@name="firstName"]') \
             .send_keys(NEW_USER_NAME)
@@ -58,18 +57,21 @@ class BCSelWebDriver(unittest.TestCase):
         self.driver.find_element(By.XPATH, '//input[@placeholder = "Type for hints..."]') \
             .send_keys(f'{NEW_USER_NAME} {NEW_USER_LASTNAME}')
         self.driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
-        time.sleep(5)
-        self.assertEqual(NEW_USER_NAME, self.driver.find_element(By.XPATH, '//div[@role="cell"][3]').text)
-        self.assertEqual(NEW_USER_LASTNAME, self.driver.find_element(By.XPATH, '//div[@role="cell"][4]').text)
-
+        time.sleep(SLEEP_INT)
+        self.assertEqual(NEW_USER_NAME,
+                         self.driver.find_element(By.XPATH, '//div[@role="cell"][3]').text)
+        self.assertEqual(NEW_USER_LASTNAME,
+                         self.driver.find_element(By.XPATH, '//div[@role="cell"][4]').text)
 
     def test_add_user_job_title(self) -> None:
         """Add missing user information in Job Details"""
+        job_title_col = '//label[text()="Job Title"]/..//following-sibling::div//i'
+
         self.driver.find_element(By.XPATH, '//span[text()="PIM"]').click()
         self.driver.find_element(By.XPATH, '//input[@placeholder = "Type for hints..."]') \
             .send_keys(f'{NEW_USER_NAME} {NEW_USER_LASTNAME}')
         self.driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
-        time.sleep(5)
+        time.sleep(SLEEP_INT)
         job_title_ext = self.driver.find_element(By.XPATH, '//div[@role="cell"][5]').text
         self.assertEqual('', job_title_ext)
 
@@ -78,47 +80,30 @@ class BCSelWebDriver(unittest.TestCase):
         self.assertTrue(self.driver.find_element(By.XPATH, '//h6[text() = "Personal Details"]'))
 
         self.driver.find_element(By.XPATH, '//a[text()= "Job"]').click()
+        self.driver.find_element(By.XPATH,job_title_col).click()
         self.driver.find_element(By.XPATH,
-                                 '//label[text()="Job Title"]/..//following-sibling::div//i').click()
-        self.driver.find_element(By.XPATH, '//div[@role="option"]//span[text()="QA Engineer"]').click()
+                                 '//div[@role="option"]//span[text()="QA Engineer"]').click()
         self.driver.find_element(By.XPATH, '//button[@type="submit"]').click()
 
         self.driver.find_element(By.XPATH, '//span[text()="PIM"]').click()
         self.driver.find_element(By.XPATH, '//input[@placeholder = "Type for hints..."]') \
             .send_keys(f'{NEW_USER_NAME} {NEW_USER_LASTNAME}')
         self.driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
-        time.sleep(5)
+        time.sleep(SLEEP_INT)
         job_title_ext = self.driver.find_element(By.XPATH, '//div[@role="cell"][5]').text
         self.assertEqual(NEW_JOB_TITLE, job_title_ext)
 
     def test_delete_user(self) -> None:
-        """
-        Scenario Delete User
-            When I go to “PIM” tab
-            And I start typing employee name in [Employee Name] field
-            Then 5 suggestions in displayed in dropdown
-            When I select user name from dropdown
-            And I click [Search] button
-            Then Row with user is presented in [Records Found]
-            When I delete the user from list of employees
-            Then Row with user is not presented in [Records Found]
-        """
+        """ Delete User """
+        dd_xpath = \
+            f'//div[@role="listbox"]//span[normalize-space()="{NEW_USER_NAME} {NEW_USER_LASTNAME}"]'
         self.driver.find_element(By.XPATH, '//span[text()="PIM"]').click()
 
         self.driver.find_element(By.XPATH, '//input[@placeholder = "Type for hints..."]') \
-            .send_keys(f'{NEW_USER_NAME}'[:1])
-        time.sleep(5)
+            .send_keys(f'{NEW_USER_NAME}'[:3])
+        time.sleep(SLEEP_INT)
 
-        # self.driver.find_element(By.XPATH,
-        #self.driver.find_element(By.XPATH, '//div[@role="listbox"]//span[.="Petr Ivanoff"]').click()
-        #self.driver.find_element(By.XPATH, '//span[contains(text(),"Petr Ivanoff")]').click()
-        #ActionChains(driver=self.driver)\
-        #    .move_to_element(self.driver.find_element(By.XPATH,'//div[@role="listbox"]')).perform()
-        ActionChains(driver=self.driver) \
-            .move_to_element(self.driver.find_element(By.XPATH, '//div[@role="option"]//span[text()="Petr Ivanoff"]')).click().perform()
-        #self.driver.find_element(By.XPATH, '//div[@role="listbox"]').
-        #self.driver.find_element(By.XPATH, '//span[.="Petr Ivanoff"]').click()
-        
+        self.driver.find_element(By.XPATH, dd_xpath).click()
         self.driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
         self.assertTrue(self.driver.find_element(By.XPATH, '//span[text() = "(1) Record Found"]'))
 
@@ -129,13 +114,10 @@ class BCSelWebDriver(unittest.TestCase):
         self.driver.find_element(By.XPATH, '//input[@placeholder = "Type for hints..."]') \
             .send_keys(f'{NEW_USER_NAME} {NEW_USER_LASTNAME}')
         self.driver.find_element(By.XPATH, '//button[@type = "submit"]').click()
-        time.sleep(5)
-        test_txt_value = self.driver.find_element(By.XPATH, '//span[text() = "No Records Found"]').text
-        self.assertEqual(NO_SEARCH_RERSULTS, test_txt_value)
-
-    def find_xp(self, xpath):
-        """Exception handler"""
-        return self.driver.find_element(By.XPATH, xpath)
+        time.sleep(SLEEP_INT)
+        test_txt_value = self.driver.find_element(By.XPATH,
+                                                  '//span[text() = "No Records Found"]').text
+        self.assertEqual(NO_SEARCH_RESULTS, test_txt_value)
 
 
 if __name__ == '__main__':
